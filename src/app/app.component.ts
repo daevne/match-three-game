649ad38d40c3081962a6e0ebd5ebc2b5898caea9
daevne <bocsoeszti@gmail.com>
2024-11-13 16:53:59 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameTableComponent } from './components/game-table/game.table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GameTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
